cc3985e044334d6310da7f8a2b325a22194fe73c
Serhiy Hull <canyonserg34@gmail.com>
2024-05-10 19:24:02 +0300

    Добавлення файлів hello_world.py, motorcycles.py, names.py

---
+++ part 1/familiarity_with_lists/motorcycles.py
@@ -0,0 +1,39 @@
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+#
+# del motorcycles[1]
+# print(motorcycles)
+
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+# print(motorcycles)
+#
+# ppoped_motorcycles = motorcycles.pop()
+# print(motorcycles)
+# print(ppoped_motorcycles)
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+
+# last_owned = motorcycles.pop()
+# print(f"The lastt motorcycle I owned was a {last_owned.title()}.")
+
+
+# motorcycles = ['honda', 'yamaha', 'suzuki']
+#
+# first_owned = motorcycles.pop(0)
+# print(f"The first motorcycle I owned was a {first_owned.title()}")
+
+
+# motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+# print(motorcycles)
+#
+# motorcycles.remove('ducati')
+# print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
---
+++ part 1/variables_data_types/hello_world.py
@@ -0,0 +1,5 @@
+massage = "Hello Python World!"
+print(massage)
+
+massage = "Hello Python Crasg Course world!"
+print(massage)
